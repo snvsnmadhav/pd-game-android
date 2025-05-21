5e881fabf15f219d13aca688d37c1fc05c8e33ee
snvsnmadhav <109173358+snvsnmadhav@users.noreply.github.com>
2025-05-21 12:57:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+import math
+import pygame
+import numpy as np
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+# (TRUNCATED for brevity here, but will be complete in the actual file writing)
